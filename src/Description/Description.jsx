e1c506d0a234af9a7b3717672836271a92c3a7c3
Oleksandr <125139066+oleksamor@users.noreply.github.com>
2024-12-27 12:03:31 +0200

    add description css jsx

---
+++ src/Description/Description.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import s from "./Description.module.css";
+const Description = () => {
+  return (
+    <div className={s.container}>
+      <h1>Sip Happens Café</h1>
+      <p>
+        Please leave your feedback about our service by selecting one of the
+        options below.
+      </p>
+    </div>
+  );
+};
+
+export default Description;
